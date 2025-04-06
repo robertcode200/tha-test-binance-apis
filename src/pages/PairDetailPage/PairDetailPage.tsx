77ee4db3ccca111d8bf5f27205b1bb4bbd2b3ecb
robertcode200 <robertcode200@gmail.com>
2025-04-07 03:44:43 +0800

    Complete basic PairDetailPage layout.

--- src/pages/PairDetailPage/PairDetailPage.tsx
+++ src/pages/PairDetailPage/PairDetailPage.tsx
@@ -1,16 +1,21 @@
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
+import './pairDetailPage.scss';
 
 const PairDetailPage = () => {
     const { pair } = useParams<{ pair: string }>();
-    console.log(pair);
 
     return (
-        
-        <div>
-            <div>PairDetailPage</div>
-            <Link to="/">Back to Home Page</Link>
+        <div className='pair-detail-page-container'>
+            <h1 className='page-title'>Pair Detail Page</h1>
+            <div className="top-info-bar">
+                <Link className="back-to-home-page-button" to="/">Back to Home Page</Link>
+                <div className="pair-content-container">
+                    <span className="content">Pair: {pair}</span>
+                </div>
+            </div>
+            <div className="kline-chart-container">KLine Chart</div>
         </div>
     );
 };
