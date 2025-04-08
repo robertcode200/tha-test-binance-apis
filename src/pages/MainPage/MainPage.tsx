273362f2e191ff28370a22cc62c3347d73fd3376
robertcode200 <robertcode200@gmail.com>
2025-04-09 03:08:50 +0800

    Replace symbol-price-cards-container with SymbolPriceCardList Component on MainPage.

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,43 +1,13 @@
-import apiUrls from '../../apiUrls';
-import { SymbolPrice } from './interface';
-import { useFetchData } from "../../hooks";
-import SymbolPriceCard from './components/SymbolPriceCard';
+import SymbolPriceCardList from './components/SymbolPriceCardList';
 import './mainPage.scss';
 
 const MainPage = () => {
-    const {
-        data: symbolPrices,
-        isLoading,
-        error
-    } = useFetchData<SymbolPrice[]>(apiUrls.mainPage.getSymbolPriceTicker);
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>Error...</div>;
-    }
-
     return (
         <div className='main-page-container'>
             <h1 className='page-title'>Main Page</h1>
-            {renderSymbolPriceCards(symbolPrices?.slice(0, 9))}
+            <SymbolPriceCardList />
         </div>
     );
 };
-
-function renderSymbolPriceCards (symbolPrices: SymbolPrice[] | null) {
-    if(!symbolPrices) return null;
-    if(!symbolPrices.length) return null;
-
-    return (
-        <div className="symbol-price-cards-container">
-            {symbolPrices.map(
-                ({ symbol, price }) => <SymbolPriceCard key={symbol} symbol={symbol} price={price} />
-            )}
-        </div>
-    );
-}
 
 export default MainPage;
