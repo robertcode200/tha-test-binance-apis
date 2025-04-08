0e9ba75fa6941cddb579f2419b636fb605fae076
robertcode200 <robertcode200@gmail.com>
2025-04-09 02:34:26 +0800

    Complete the style tunning of Pair Detail Page.

--- src/pages/PairDetailPage/PairDetailPage.tsx
+++ src/pages/PairDetailPage/PairDetailPage.tsx
@@ -7,12 +7,10 @@ const PairDetailPage = () => {
     return (
         <div className='pair-detail-page-container'>
             <h1 className='page-title'>Pair Detail Page</h1>
-            <div className="top-info-bar">
-                <Link className="back-to-home-page-button" to="/">Back to Home Page</Link>
-            </div>
-            <div className="kline-chart-container">
-                <KLineChartContainer />
-            </div>
+
+            <Link className="back-to-home-page-button" to="/">Back to Home Page</Link>
+
+            <KLineChartContainer />
         </div>
     );
 };
