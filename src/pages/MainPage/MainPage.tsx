215b4d3e83b4c07fac782bc6347e030c493dd2bf
robertcode200 <robertcode200@gmail.com>
2025-04-09 07:21:52 +0800

    Remove unnecessary comments.

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,35 +1,7 @@
-// import { useRef, useEffect } from 'react';
 import SymbolPriceCardList from './components/SymbolPriceCardList';
 import './mainPage.scss';
 
 const MainPage = () => {
-        // const handleScroll = () => {
-        //     console.log('Height:', document.documentElement.scrollHeight);
-        // };
-    
-        // const isScrollEventListenerSet = useRef<boolean>(false);
-    
-        // const renderCount = useRef(0);
-        // const theRangerEnteredCount = useRef(0);
-    
-        // useEffect(() => {
-        //     renderCount.current += 1;
-        //     console.log('scroll effect', renderCount);
-        //     console.log(' ');
-        //     if (!isScrollEventListenerSet.current) {
-        //         theRangerEnteredCount.current += 1;
-        //         console.log('theRangerEnteredCount', theRangerEnteredCount.current);
-        //         window.addEventListener('scroll', handleScroll);
-        //         isScrollEventListenerSet.current = true;
-    
-        //     }
-    
-        //     return () => {
-    
-        //     };
-        // }, []);
-
-
     return (
         <div className='main-page-container'>
             <h1 className='page-title'>Main Page</h1>
